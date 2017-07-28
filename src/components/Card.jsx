d1492a51cd6e265542f2432bf11432471aff81e9
MINSK\Viachaslau_Vashchona <viachaslau_vashchonak@epam.com>
2017-07-28 11:47:43 +0300

    type icons and mixin

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,20 +8,19 @@ class Card extends React.Component {
       let cardData = this.props.cardData;
       
       let className = "Card-wrapper " + cardData.type + " " + cardData.theme;
-      let type = this.props.type;
       let iconClass = "icon-" + cardData.type.toLowerCase();
 
       return <div className={className}>
         <article className="Card">
-            { type == 'Meetup' ? <p className="Card-date">{cardData.date}</p> : null }
-            { type == 'Tender' ? <div className="Card-info">
+            { cardData.type == 'Meetup' ? <p className="Card-date">{cardData.date}</p> : null }
+            { cardData.type == 'Tender' ? <div className="Card-info">
                 <p className="Card-activity">{cardData.activity}</p>
                 <p className="Card-date">{cardData.date}</p>
             </div> : null }
             <h1><a href="#">{cardData.title}</a></h1>
-            { type == 'Meetup' ? <p className="Card-place">{cardData.place}</p> : null }
+            { cardData.type == 'Meetup' ? <p className="Card-place">{cardData.place}</p> : null }
             <div>
-                <p className="Card-sphere">{type !== 'Meetup' && type !== 'Tender' ? cardData.sphere : ''}</p>
+                <p className="Card-sphere">{cardData.type !== 'Meetup' && cardData.type !== 'Tender' ? cardData.sphere : ''} <i className={iconClass}></i></p>
                 <p className="Card-author">
                     {cardData.author.name}
                     <span>{cardData.author.descr}</span>
