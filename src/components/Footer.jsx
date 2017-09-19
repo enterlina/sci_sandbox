e3f19688217b78011b13b95a24514fdd37983eea
enterlina <enterlina@gmail.com>
2017-09-19 11:14:40 +0300

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,26 +49,7 @@ class Footer extends React.Component {
                     </div>
                     <div className="Footer-grid-third-column">
                         <ul className="Footer-social-networks layout-container" role="navigation">
-                            <li><!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
-<style type="text/css">
-	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
-	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
-	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
-<div id="mc_embed_signup">
-<form action="//sciencehit.us14.list-manage.com/subscribe/post?u=85e324d7c30f904f1f8571443&amp;id=0886f4ed18" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	<label for="mce-EMAIL">Подписаться на рассылку SciTech</label>
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_85e324d7c30f904f1f8571443_0886f4ed18" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-    </div>
-</form>
-</div>
-
-<!--End mc_embed_signup--></li>
+                          
                             <li><a href="https://www.facebook.com/scitechby/" target="_blank">Facebook</a></li>
                             <li><a href="https://vk.com/scitechby" target="_blank">{this.props.lang.VK || 'VK'}</a></li>
                             <li><a href="https://www.youtube.com/channel/UClF9SlczBDDEbB9JxKCVaBw" target="_blank">YouTube</a></li>
