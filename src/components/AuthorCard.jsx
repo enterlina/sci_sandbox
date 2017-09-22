31fbdcdcc1319177f01dd5f84a40d5d67252d993
sergeitoptalo <toptalo@hotmail.com>
2017-09-22 11:48:47 +0300

    remove extra code

--- src/components/AuthorCard.jsx
+++ src/components/AuthorCard.jsx
@@ -6,31 +6,7 @@ import {Link} from 'react-router-dom';
 require("!style-loader!css-loader!sass-loader!./AuthorCard.scss");
 
 class AuthorCard extends React.Component {
- /*  constructor() {
-    super();
-    this.authorExternalLink = null;
-    this.externalLinkDomainName = null;
-  }
- */
-  /* componentWillMount() {
-    let authorDescription = this.props.data.description;
-    let linksResolver = /(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?/;
-    let domainNameResolver = /((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})/;
-    authorDescription.map((description) => {
-      for (let key in description) {
-        let link = description[key].match(linksResolver);
-        if (link) {
-          this.authorExternalLink = link[0];
-          this.externalLinkDomainName = link[0].match(domainNameResolver)[0];
-          description[key] = description[key].replace(linksResolver,``);
-        }
-      }
-    })
-  } */
-
   render() {
-   // let linksResolver = /(http?s?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w\.-]*)*\/?/;
-   // let domainNameResolver = /((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})/;
     let author = this.props.data;
     let lang = this.props.lang;
     let contacts = [];
@@ -39,15 +15,6 @@ class AuthorCard extends React.Component {
     }
 
     let authorDescription = langArrayHandler(author.description, lang)
-
-    /* let link = authorDescription.match(linksResolver);
-    
-    if (link) {
-      let authorExternalLink = link[0];
-      let externalLinkDomainName = authorExternalLink.match(domainNameResolver)[0];
-      let newLink = `<a href=${authorExternalLink} target="_blank">${lang == 'ru' ? 'Больше информации на ' : 'More info on '}${externalLinkDomainName}</a>`;
-      authorDescription = authorDescription.replace(linksResolver, newLink);
-    } */
 
     return <div className="AuthorCard">
       <h2>{ author.isCustom  ? langArrayHandler(author.name, lang) : <Link to={'/People/' + author._id}>{langArrayHandler(author.name, lang)}</Link> }</h2>
